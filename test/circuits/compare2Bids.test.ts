bb2ecee713a750340286f793d909288f4587599b
Andrew Morris <voltrevo@gmail.com>
2025-07-28 11:06:48 +1000

    start tests for compare2Bids

--- test/circuits/compare2Bids.test.ts
+++ test/circuits/compare2Bids.test.ts
@@ -1,9 +1,20 @@
+import fs from 'fs';
 import * as summon from 'summon-ts';
+import getProjectRoot from '../../src/util/getProjectRoot';
+import { strict as assert } from 'assert';
 
 describe('compare2Bids', () => {
-  it('compiles (TODO)', async () => {
+  it('compiles', async () => {
     await summon.init();
 
-    // const {} = summon.compile()
+    const { circuit: _circuit, diagnostics } = summon.compile({
+      path: `${getProjectRoot()}/src/circuits/compare2Bids.ts`,
+      boolifyWidth: 32,
+      readFile: (filePath) => fs.readFileSync(filePath, 'utf-8'),
+    });
+
+    for (const [path, diag] of Object.entries(diagnostics)) {
+      assert.equal(diag.length, 0, `${path}: ${JSON.stringify(diag, null, 2)}`);
+    }
   });
 });
